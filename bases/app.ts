dd71eda9bef15d489a24de526cf0abb97e78b25e
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2026-09-15 17:54:37 -0300

    bases - tipos

--- bases/app.ts
+++ bases/app.ts
@@ -1,10 +1,40 @@
-const msg: string = "Hola mundo";
+(() => {
+  // Tipos
+  const batman: string = "Bruce";
+  const superman: string = "Clark";
 
-const hero = {
-  name: "Ironman",
-  age: 45,
-};
+  const existe: boolean = false;
 
-hero.age = 50;
+  // Tuplas
+  const parejaHeroes: [string, string] = [batman, superman];
+  const villano: [string, number, boolean] = ["Lex Lutor", 5, true];
 
-console.log(hero.age + 1);
+  // Arreglos
+  const aliados: string[] = ["Mujer Maravilla", "Acuaman", "San", "Flash"];
+
+  //Enumeraciones
+  enum fuerza {
+    fuerzaFlash = 5,
+    fuerzaSuperman = 100,
+    fuerzaBatman = 1,
+    fuerzaAcuaman = 0,
+  }
+  const fuerzaFlash: fuerza = fuerza.fuerzaFlash;
+  const fuerzaSuperman: fuerza = fuerza.fuerzaSuperman;
+  const fuerzaBatman: fuerza = fuerza.fuerzaBatman;
+  const fuerzaAcuaman: fuerza = fuerza.fuerzaAcuaman;
+
+  // Retorno de funciones
+  function activar_batiseñal(): string {
+    return "activada";
+  }
+
+  function pedir_ayuda(): void {
+    console.log("Auxilio!!!");
+  }
+
+  // Aserciones de Tipo
+  const poder: any = "100";
+  const largoDelPoder: number = (poder as string).length;
+  console.log(largoDelPoder);
+})();
