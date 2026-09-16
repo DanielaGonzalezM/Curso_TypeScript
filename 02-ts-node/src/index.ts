ff7a2a9626af70c9d07e208020cdb14d68a5ff26
DanielaGonzalezM <daniela.gonzalez.m02@mail.pucv.cl>
2026-09-16 19:53:27 -0300

    Fin - NameSpace

--- 02-ts-node/src/index.ts
+++ 02-ts-node/src/index.ts
@@ -1,10 +1,8 @@
+import { Hero as SuperHero} from "./classes/Hero";
+import powers from "./data/powers";
 
 
-console.log('Hola Mundo!--');
-
-
-
-
-
-
-
+const ironman = new SuperHero("Ironman", 1, 55);
+console.log(ironman);
+console.log("Hola " + ironman.name);
+console.log(powers);
